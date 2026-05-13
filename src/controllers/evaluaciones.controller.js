cd9a25fa4c62d87cc6099ed5f2b731fddfdbafb0
Render12345 <manriquezjorge100@gmail.com>
2026-05-13 07:08:40 -0600

    feat: implementa controladores y servicios para equipos, evaluaciones y exposiciones

--- src/controllers/evaluaciones.controller.js
+++ src/controllers/evaluaciones.controller.js
@@ -3,9 +3,45 @@ const evaluacionesService = require('../services/evaluaciones.service');
 // TODO: Implementar - POST /api/v1/evaluaciones
 const create = async (req, res, next) => {
     try {
-        // TODO: obtener id_exposicion, id_alumno_evaluador y detalles de req.body y llamar evaluacionesService.create
-        // TODO: validar que detalles sea un array con id_criterio y calificacion
-        // TODO: validar que calificacion este entre 0 y 10
+        const { id_exposicion, id_alumno_evaluador, detalles } = req.body;
+
+        if (!id_exposicion || !id_alumno_evaluador) {
+            const err = new Error('id_exposicion e id_alumno_evaluador son requeridos');
+            err.status = 400;
+            err.error  = 'Bad Request';
+            return next(err);
+        }
+
+        if (!Array.isArray(detalles) || detalles.length === 0) {
+            const err = new Error('detalles debe ser un array con al menos un criterio');
+            err.status = 400;
+            err.error  = 'Bad Request';
+            return next(err);
+        }
+
+        // Validar estructura de cada detalle
+        for (const d of detalles) {
+            if (d.id_criterio === undefined || d.calificacion === undefined) {
+                const err = new Error('Cada detalle debe tener id_criterio y calificacion');
+                err.status = 400;
+                err.error  = 'Bad Request';
+                return next(err);
+            }
+            if (d.calificacion < 0 || d.calificacion > 10) {
+                const err = new Error('La calificacion debe estar entre 0 y 10');
+                err.status = 400;
+                err.error  = 'Bad Request';
+                return next(err);
+            }
+        }
+
+        const data = await evaluacionesService.create({
+            id_exposicion:       parseInt(id_exposicion),
+            id_alumno_evaluador: parseInt(id_alumno_evaluador),
+            detalles,
+        });
+        res.status(201).json(data);
+
     } catch (err) {
         next(err);
     }
@@ -14,7 +50,9 @@ const create = async (req, res, next) => {
 // TODO: Implementar - GET /api/v1/evaluaciones/:id
 const getById = async (req, res, next) => {
     try {
-        // TODO: obtener id de req.params y llamar evaluacionesService.getById
+        const id   = parseInt(req.params.id);
+        const data = await evaluacionesService.getById(id);
+        res.status(200).json(data);
     } catch (err) {
         next(err);
     }
